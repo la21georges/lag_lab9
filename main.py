36cd4b4d9c85994f946b032105ac269d259f1b47
la21georges <lrncgeorges@gmail.com>
2024-04-10 12:40:35 -0400

    # create the encoder function

---
+++ main.py
@@ -0,0 +1,9 @@
+def encoder(password_string):
+    new_password = ""
+    for digit in password_string:
+        digit = int(digit)
+        digit += 3
+        new_password += str(digit)
+    return new_password
+
+print(encoder("12345555"))
