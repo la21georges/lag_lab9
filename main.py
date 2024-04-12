cc6e6cb8f3761e82dfec48552aff993ecdf1cb24
T1Johnson <166639264+T1Johnson@users.noreply.github.com>
2024-04-12 14:16:31 -0400

    decoder

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def encoder(password_string):
     return new_password
 
 print(encoder("12345555"))
+
+def decoder(password_string):
+    new_password = ""
+    for digit in password_string:
+        digit = int(digit)
+        digit -= 3
+        new_password -= str(digit)
+    return new_password
